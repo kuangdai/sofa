dc41569b162c9cbdd17ab6ef28a46121b75277a1
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-05-17 19:29:22 +0100

    Update net.py

--- src/net.py
+++ src/net.py
@@ -5,14 +5,14 @@ import torch.nn as nn
 class SofaNet(nn.Module):
     def __init__(self, ab, hidden_sizes):
         super().__init__()
-        self.na, self.nb = ab.shape[0], ab.shape[1]
+        self.n_ab = ab.shape[0]
 
         # weights
         weights, biases = [], []
         hidden_sizes = [1] + hidden_sizes + [2]  # 1 for alpha, 2 for a, b
         for i in range(len(hidden_sizes) - 1):
-            weights.append(torch.randn(self.na, self.nb, hidden_sizes[i + 1], hidden_sizes[i]))
-            biases.append(torch.zeros(self.na, self.nb, hidden_sizes[i + 1]))
+            weights.append(torch.randn(self.n_ab, hidden_sizes[i + 1], hidden_sizes[i]))
+            biases.append(torch.zeros(self.n_ab, hidden_sizes[i + 1]))
 
         # initial condition
         weights[-1][:] = 0.
@@ -24,7 +24,7 @@ class SofaNet(nn.Module):
 
     def forward(self, alpha, compute_gradients=False):
         # expand size of alpha
-        alpha = alpha[None, None, :, None].expand(self.na, self.nb, -1, -1)
+        alpha = alpha[None, :, None].expand(self.n_ab, -1, -1)
 
         # zcs
         z = torch.tensor(0., requires_grad=True, device=alpha.device)
@@ -36,9 +36,9 @@ class SofaNet(nn.Module):
 
         # layers
         for w, b in zip(self.weights[:-1], self.biases[:-1]):
-            x = torch.einsum("MNij,MNBj->MNBi", w, x) + b[:, :, None, :]
+            x = torch.einsum("Nij,NBj->NBi", w, x) + b[:, None, :]
             x = torch.relu(x)
-        x = torch.einsum("MNij,MNBj->MNBi", self.weights[-1], x) + self.biases[-1][:, :, None, :]
+        x = torch.einsum("Nij,NBj->NBi", self.weights[-1], x) + self.biases[-1][:, None, :]
 
         # gradient
         if compute_gradients:
