931c8a8969bbde4eff9bf07c473a3280d5cd2560
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-05-17 18:45:21 +0100

    Update net.py

--- src/net.py
+++ src/net.py
@@ -3,23 +3,28 @@ import torch.nn as nn
 
 
 class SofaNet(nn.Module):
-    def __init__(self, n_nets, hidden_sizes, ab_lower=0.05, ab_upper=1.05):
+    def __init__(self, ab, hidden_sizes):
         super().__init__()
-        weights = []
-        biases = []
+        self.na, self.nb = ab.shape[0], ab.shape[1]
+
+        # weights
+        weights, biases = [], []
         hidden_sizes = [1] + hidden_sizes + [2]  # 1 for alpha, 2 for a, b
         for i in range(len(hidden_sizes) - 1):
-            weights.append(torch.randn((n_nets, hidden_sizes[i + 1], hidden_sizes[i])))
-            biases.append(torch.zeros(n_nets, hidden_sizes[i + 1]))
-        self.n_nets = n_nets
+            weights.append(torch.randn(self.na, self.nb, hidden_sizes[i + 1], hidden_sizes[i]))
+            biases.append(torch.zeros(self.na, self.nb, hidden_sizes[i + 1]))
+
+        # initial condition
+        weights[-1][:] = 0.
+        biases[-1][:] = ab[:]
+
+        # trainable
         self.weights = nn.ParameterList(weights)
         self.biases = nn.ParameterList(biases)
-        self.ab_lower = ab_lower
-        self.ab_upper = ab_upper
 
     def forward(self, alpha, compute_gradients=False):
         # expand size of alpha
-        alpha = alpha[None, :, None].expand(self.n_nets, -1, -1)
+        alpha = alpha[None, None, :, None].expand(self.na, self.nb, -1, -1)
 
         # zcs
         z = torch.tensor(0., requires_grad=True, device=alpha.device)
@@ -31,13 +36,9 @@ class SofaNet(nn.Module):
 
         # layers
         for w, b in zip(self.weights[:-1], self.biases[:-1]):
-            x = torch.einsum("nij,nbj->nbi", w, x) + b[:, None, :]
+            x = torch.einsum("MNij,MNBj->MNBi", w, x) + b[:, None, :]
             x = torch.relu(x)
-        x = torch.einsum("nij,nbj->nbi", self.weights[-1], x) + self.biases[-1][:, None, :]
-
-        # range
-        x = torch.sigmoid(x)
-        x = self.ab_lower + (self.ab_upper - self.ab_lower) * x
+        x = torch.einsum("MNij,MNBj->MNBi", self.weights[-1], x) + self.biases[-1][:, None, :]
 
         # gradient
         if compute_gradients:
