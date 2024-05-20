49335fae026852b94e92e00be6041aa1bc945f18
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-05-20 21:20:25 +0100

    Update net.py

--- src/net.py
+++ src/net.py
@@ -3,8 +3,9 @@ import torch.nn as nn
 
 
 class SofaNetEllipse(nn.Module):
-    def __init__(self, ab0, hidden_sizes):
+    def __init__(self, ab0, hidden_sizes, xy_correction):
         super().__init__()
+        self.xy_correction = xy_correction
         self.fcs_ab = nn.ModuleList()  # layers for a, b
         self.fcs_kxy = nn.ModuleList()  # layers for k applied to xp, yp
         self.fcs_sxy = nn.ModuleList()  # layers for s applied to xp, yp
@@ -13,18 +14,20 @@ class SofaNetEllipse(nn.Module):
         hidden_sizes = [1] + hidden_sizes + [2]
         for i in range(len(hidden_sizes) - 1):
             self.fcs_ab.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]))
-            self.fcs_kxy.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]))
-            self.fcs_sxy.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]))
+            if xy_correction:
+                self.fcs_kxy.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]))
+                self.fcs_sxy.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]))
 
         # initialized so that output is [a, b]
         self.fcs_ab[-1].weight.data[:] = 0.
         self.fcs_ab[-1].bias.data[:] = ab0
-        # initialized so that output is [1, 1]
-        self.fcs_kxy[-1].weight.data[:] = 0.
-        self.fcs_kxy[-1].bias.data[:] = 1.
-        # initialized so that output is [0, 0]
-        self.fcs_sxy[-1].weight.data[:] = 0.
-        self.fcs_sxy[-1].bias.data[:] = 0.
+        if xy_correction:
+            # initialized so that output is [1, 1]
+            self.fcs_kxy[-1].weight.data[:] = 0.
+            self.fcs_kxy[-1].bias.data[:] = 1.
+            # initialized so that output is [0, 0]
+            self.fcs_sxy[-1].weight.data[:] = 0.
+            self.fcs_sxy[-1].bias.data[:] = 0.
 
     def forward(self, alpha):
         n = len(alpha)
@@ -38,21 +41,26 @@ class SofaNetEllipse(nn.Module):
         alpha = alpha + z
 
         # layers
-        ab, kxy, sxy = alpha[:, None], alpha[:, None], alpha[:, None]
-        for fc_ab, fc_kxy, fc_sxy in zip(self.fcs_ab[:-1], self.fcs_kxy[:-1], self.fcs_sxy[:-1]):
+        ab = alpha[:, None]
+        for fc_ab in self.fcs_ab[:-1]:
             ab = torch.tanh(fc_ab(ab))
-            kxy = torch.tanh(fc_kxy(kxy))
-            sxy = torch.tanh(fc_sxy(sxy))
         ab = self.fcs_ab[-1](ab)
-        kxy = self.fcs_kxy[-1](kxy)
-        sxy = self.fcs_sxy[-1](sxy)
 
-        # curve p
+        # curve p by ellipse
         a, b = ab[:, 0], ab[:, 1]
         xp = a * (torch.cos(alpha) - 1)
         yp = b * torch.sin(alpha)
-        xp = xp * kxy[:, 0] + sxy[:, 0]
-        yp = yp * kxy[:, 1] + sxy[:, 1]
+
+        # xy correction
+        if self.xy_correction:
+            kxy, sxy = alpha[:, None], alpha[:, None]
+            for fc_kxy, fc_sxy in zip(self.fcs_kxy[:-1], self.fcs_sxy[:-1]):
+                kxy = torch.tanh(fc_kxy(kxy))
+                sxy = torch.tanh(fc_sxy(sxy))
+            kxy = self.fcs_kxy[-1](kxy)
+            sxy = self.fcs_sxy[-1](sxy)
+            xp = xp * kxy[:, 0] + sxy[:, 0]
+            yp = yp * kxy[:, 1] + sxy[:, 1]
 
         # gradient by zcs
         dummy = torch.ones_like(xp, requires_grad=True)
