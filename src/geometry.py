95901abaac6184cc23e0282e615b7ceed6866d51
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-05-20 21:05:53 +0100

    Update geometry.py

--- src/geometry.py
+++ src/geometry.py
@@ -25,7 +25,7 @@ def split_curve(x, y):
     return x_splits, y_splits
 
 
-def interp1d_sorted(x0, y0, x1, outside_value=0.):
+def interp1d_sorted(x0, y0, x1, left_value, right_value):
     assert len(x0) >= 2
 
     # flip if x0 is descending
@@ -46,19 +46,19 @@ def interp1d_sorted(x0, y0, x1, outside_value=0.):
     y1 = ya + k * (x1 - xa)
 
     # mask those out of range
-    y1 = torch.where(torch.logical_and(torch.greater_equal(x1, xa),
-                                       torch.less_equal(x1, xb)), y1, outside_value)
+    y1 = torch.where(torch.less(x1, x0[0]), left_value, y1)
+    y1 = torch.where(torch.greater(x1, x0[-1]), right_value, y1)
     return y1
 
 
-def interp1d_multi_section(x, y, x_tar, outside_value, min_split_reduce):
+def interp1d_multi_section(x, y, x_tar, left_value, right_value, min_split_reduce):
     # split curve
     x_splits, y_splits = split_curve(x, y)
     # loop over splits
     # TODO: this may be vectorised if we pad splits to same length; still, hard to implement
     out = torch.empty((len(x_splits), len(x_tar)), device=x_tar.device)
     for j, (x_split, y_split) in enumerate(zip(x_splits, y_splits)):
-        out[j] = interp1d_sorted(x_split, y_split, x_tar, outside_value=outside_value)
+        out[j] = interp1d_sorted(x_split, y_split, x_tar, left_value, right_value)
     # reduce splits
     minmax = torch.min if min_split_reduce else torch.max
     out = minmax(out, dim=0)[0]
@@ -71,8 +71,8 @@ def compute_area(alpha, xp, yp, xp_prime, yp_prime, n_area_samples=2000, return_
     yu = yp + (1 + torch.cos(alpha)) * xp_prime + torch.sin(alpha) * yp_prime
     # handle the case where u is a point at zero, which may cause nan
     if torch.isclose(xu, torch.zeros_like(xu)).all() and torch.isclose(yu, torch.zeros_like(yu)).all():
-        xu[:] = 0.
-        yu[:] = 0.
+        xu = torch.zeros_like(xu)
+        yu = torch.zeros_like(yu)
 
     # curve q
     sqrt2 = torch.sqrt(torch.tensor(2., device=alpha.device))
@@ -89,14 +89,14 @@ def compute_area(alpha, xp, yp, xp_prime, yp_prime, n_area_samples=2000, return_
     x_sample = center + x_sample * (1. - center)
 
     # lower edge
-    y_sample_p = interp1d_multi_section(xp, yp, x_sample, outside_value=0., min_split_reduce=False)
-    y_sample_u = interp1d_multi_section(xu, yu, x_sample, outside_value=0., min_split_reduce=False)
+    y_sample_p = interp1d_multi_section(xp, yp, x_sample, left_value=0., right_value=0., min_split_reduce=False)
+    y_sample_u = interp1d_multi_section(xu, yu, x_sample, left_value=0., right_value=0., min_split_reduce=False)
     y_sample_lower = torch.maximum(y_sample_p, y_sample_u)
     y_sample_lower = torch.clamp(y_sample_lower, min=0., max=1.)
 
     # upper edge
-    y_sample_q = interp1d_multi_section(xq, yq, x_sample, outside_value=1., min_split_reduce=True)
-    y_sample_v = interp1d_multi_section(xv, yv, x_sample, outside_value=1., min_split_reduce=True)
+    y_sample_q = interp1d_multi_section(xq, yq, x_sample, left_value=1., right_value=0., min_split_reduce=True)
+    y_sample_v = interp1d_multi_section(xv, yv, x_sample, left_value=1., right_value=0., min_split_reduce=True)
     y_sample_upper = torch.minimum(y_sample_q, y_sample_v)
     y_sample_upper = torch.clamp(y_sample_upper, min=0., max=1.)
 
