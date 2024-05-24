2763d0e70ee0c310c0411748d6426bab7dc5eb3e
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-05-24 18:09:38 +0100

    Update train.py

--- train.py
+++ train.py
@@ -1,29 +1,28 @@
 import argparse
 
-import matplotlib.pyplot as plt
 import torch
 from tqdm import trange
 
 from src.geometry import compute_area
-from src.net import SofaNetEllipse
+from src.net import SofaNet
 
 torch.set_default_dtype(torch.float64)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("Pretrain ellipse",
+    parser = argparse.ArgumentParser("Moving sofa",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-v", "--velocity-mode", action="store_true",
+                        help="velocity mode")
+    parser.add_argument("-t", "--n-times", type=int,
+                        default=1000, help="number of times")
+    parser.add_argument("-a", "--n-areas", type=int,
+                        default=2000, help="number of x's for area integration")
     parser.add_argument("--a0", type=float,
-                        default=0.5, help="initial value of a")
+                        default=0.5, help="a0 for path initialization")
     parser.add_argument("--b0", type=float,
-                        default=0.6, help="initial value of b")
-    parser.add_argument("--xy-correction", action="store_true",
-                        help="whether to perform xy correction")
+                        default=0.6, help="b0 for path initialization")
     parser.add_argument("--hidden-sizes", type=int, nargs="+",
-                        default=[128, 128, 128], help="hidden sizes of model")
-    parser.add_argument("--n-alphas", type=int,
-                        default=501, help="number of alphas in [0, pi]")
-    parser.add_argument("--n-area-samples", type=int,
-                        default=1000, help="number of x's for area calculation")
+                        default=[128, 128, 128, 128], help="hidden sizes of model")
     parser.add_argument("--lr", type=float,
                         default=1e-4, help="learning rate")
     parser.add_argument("--lr-decay-rate", type=float,
@@ -34,30 +33,26 @@ if __name__ == "__main__":
                         default=1000, help="number of epochs")
     parser.add_argument("--device", type=str,
                         default="cpu", help="training device")
+    parser.add_argument("--name", type=str,
+                        default="last", help="name of running")
     args = parser.parse_args()
-    name = "with_xy_correction" if args.xy_correction else "no_xy_correction"
-
-    # a and b
-    ab0 = torch.tensor((args.a0, args.b0))
 
     # model
-    model = SofaNetEllipse(ab0, hidden_sizes=args.hidden_sizes, xy_correction=args.xy_correction).to(args.device)
+    model = SofaNet(n_in=args.n_times, n_t=args.n_times, hidden_sizes=args.hidden_sizes,
+                    a0=args.a0, b0=args.b0, velocity_mode=args.velocity_mode).to(args.device)
     optimizer = torch.optim.AdamW(model.parameters(), lr=args.lr, maximize=True)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.lr_decay_step, gamma=args.lr_decay_rate)
-
-    # alpha
-    t = torch.linspace(0, 1., args.n_alphas).to(args.device)
 
     # train
     largest_area = -1.
     progress_bar = trange(args.epochs)
     for epoch in progress_bar:
-        alpha, xp, yp, dt_alpha, dt_xp, dt_yp = model.forward(t)
+        t, alpha, xp, yp, dt_alpha, dt_xp, dt_yp = model.forward()
         area = compute_area(t, alpha, xp, yp, dt_alpha, dt_xp, dt_yp, n_area_samples=args.n_area_samples)
         if area > largest_area:
             # checkpoint best
             largest_area = area.item()
-            torch.save(model.state_dict(), f"outputs/best_model_{name}.pt")
+            torch.save(model.state_dict(), f"outputs/best_model_{args.name}.pt")
         optimizer.zero_grad()
         area.backward()
         optimizer.step()
@@ -66,12 +61,9 @@ if __name__ == "__main__":
 
     # eval
     if largest_area >= 0.:
-        model.load_state_dict(torch.load(f"outputs/best_model_{name}.pt"))
-    alpha, xp, yp, dt_alpha, dt_xp, dt_yp = model.forward(t)
-    area, outline = compute_area(t, alpha, xp, yp, dt_alpha, dt_xp, dt_yp,
-                                 n_area_samples=args.n_area_samples, return_outline=True)
+        model.load_state_dict(torch.load(f"outputs/best_model_{args.name}.pt"))
+    t, alpha, xp, yp, dt_alpha, dt_xp, dt_yp = model.forward()
+    area, gg = compute_area(t, alpha, xp, yp, dt_alpha, dt_xp, dt_yp,
+                            n_area_samples=args.n_area_samples, return_geometry=True)
+    torch.save(gg, f"outputs/best_geometry_{args.name}.pt")
     print("Largest area found:", area.item())
-    plt.figure(dpi=200)
-    plt.plot(outline[0], outline[1])
-    plt.plot(outline[0], outline[2])
-    plt.savefig(f"outputs/outline_{name}.png")
