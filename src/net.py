9cfbc9207f4adb455da37dbd3f7e4914726778f6
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-05-24 17:53:52 +0100

    Update net.py

--- src/net.py
+++ src/net.py
@@ -2,69 +2,70 @@ import torch
 import torch.nn as nn
 
 
-class SofaNetEllipse(nn.Module):
-    def __init__(self, ab0, hidden_sizes, xy_correction):
+class SofaNet(nn.Module):
+    def __init__(self, n_in, n_t, hidden_sizes, a0=0.5, b0=0.6, velocity_mode=True):
         super().__init__()
-        self.xy_correction = xy_correction
-        self.fcs_ab = nn.ModuleList()  # layers for a, b
-        self.fcs_kxy = nn.ModuleList()  # layers for k applied to xp, yp
-        self.fcs_sxy = nn.ModuleList()  # layers for s applied to xp, yp
+        self.n_in, self.n_t = n_in, n_t
+        self.velocity_mode = velocity_mode
+        n_out = n_t + 2 if velocity_mode else n_t
 
-        # [1] for alpha, [2] for [a, b], [k_xp, k_yp], [s_xp, s_yp]
-        hidden_sizes = [1] + hidden_sizes + [2]
+        # network
+        self.fcs_alpha = nn.ModuleList()
+        self.fcs_xp = nn.ModuleList()
+        self.fcs_yp = nn.ModuleList()
+        hidden_sizes = [n_in] + hidden_sizes + [n_out]
         for i in range(len(hidden_sizes) - 1):
-            self.fcs_ab.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]))
-            if xy_correction:
-                self.fcs_kxy.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]))
-                self.fcs_sxy.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]))
+            self.fcs_alpha.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]))
+            self.fcs_xp.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]))
+            self.fcs_yp.append(nn.Linear(hidden_sizes[i], hidden_sizes[i + 1]))
 
-        # initialized so that output is [a, b]
-        self.fcs_ab[-1].weight.data[:] = 0.
-        self.fcs_ab[-1].bias.data[:] = ab0
-        if xy_correction:
-            # initialized so that output is [1, 1]
-            self.fcs_kxy[-1].weight.data[:] = 0.
-            self.fcs_kxy[-1].bias.data[:] = 1.
-            # initialized so that output is [0, 0]
-            self.fcs_sxy[-1].weight.data[:] = 0.
-            self.fcs_sxy[-1].bias.data[:] = 0.
+        # initialized to an elliptical path
+        d_alpha = 0 if velocity_mode else torch.pi / 2 / (n_t - 1)
+        alpha = torch.linspace(-d_alpha, torch.pi / 2 + d_alpha, n_out)
+        if velocity_mode:
+            initial_alpha = torch.ones(n_t) * torch.pi / 2
+            initial_xp = a0 * (-torch.sin(2 * alpha)) * torch.pi
+            initial_yp = b0 * torch.cos(2 * alpha) * torch.pi
+        else:
+            initial_alpha = alpha
+            initial_xp = a0 * (torch.cos(2 * alpha) - 1)
+            initial_yp = b0 * torch.sin(2 * alpha)
+        self.fcs_alpha[-1].weight.data[:] = 0.
+        self.fcs_xp[-1].weight.data[:] = 0.
+        self.fcs_yp[-1].weight.data[:] = 0.
+        self.fcs_alpha[-1].bias.data[:] = initial_alpha
+        self.fcs_xp[-1].bias.data[:] = initial_xp
+        self.fcs_yp[-1].bias.data[:] = initial_yp
 
-    def forward(self, t):
-        # zcs
-        z = torch.tensor(0., requires_grad=True, device=t.device)
-        t = z + t
+    def forward(self):
+        # forward
+        # value of x is not important
+        device = self.fcs_xp[-1].weight.device
+        x = torch.linspace(-1, 1., self.n_in, device=device)
+        x_alpha, x_xp, x_yp = x, x, x
+        for fc_alpha, fc_xp, fc_yp in zip(self.fcs_alpha[:-1], self.fcs_xp[:-1], self.fcs_yp[:-1]):
+            x_alpha = torch.relu(fc_alpha(x_alpha))
+            x_xp = torch.relu(fc_xp(x_xp))
+            x_yp = torch.relu(fc_yp(x_yp))
+        x_alpha = self.fcs_alpha[-1](x_alpha)
+        x_xp = self.fcs_xp[-1](x_xp)
+        x_yp = self.fcs_yp[-1](x_yp)
 
-        # alpha
-        alpha = t * torch.pi / 2.
-
-        # layers
-        ab = 2 * alpha[:, None]
-        for fc_ab in self.fcs_ab[:-1]:
-            ab = torch.tanh(fc_ab(ab))
-        ab = self.fcs_ab[-1](ab)
-
-        # curve p by ellipse
-        a, b = ab[:, 0], ab[:, 1]
-        xp = a * (torch.cos(2 * alpha) - 1)
-        yp = b * torch.sin(2 * alpha)
-
-        # xy correction
-        if self.xy_correction:
-            kxy, sxy = 2 * alpha[:, None], 2 * alpha[:, None]
-            for fc_kxy, fc_sxy in zip(self.fcs_kxy[:-1], self.fcs_sxy[:-1]):
-                kxy = torch.tanh(fc_kxy(kxy))
-                sxy = torch.tanh(fc_sxy(sxy))
-            kxy = self.fcs_kxy[-1](kxy)
-            sxy = self.fcs_sxy[-1](sxy)
-            xp = xp * kxy[:, 0] + sxy[:, 0]
-            yp = yp * kxy[:, 1] + sxy[:, 1]
-
-        # gradient by zcs
-        dummy = torch.ones_like(xp, requires_grad=True)
-        omega_xp = (dummy * xp).sum()
-        omega_yp = (dummy * yp).sum()
-        omega_xp_z = torch.autograd.grad(omega_xp, z, create_graph=True)[0]
-        omega_yp_z = torch.autograd.grad(omega_yp, z, create_graph=True)[0]
-        xp_prime = torch.autograd.grad(omega_xp_z, dummy, create_graph=True)[0]
-        yp_prime = torch.autograd.grad(omega_yp_z, dummy, create_graph=True)[0]
-        return alpha, xp, yp, torch.ones_like(t) * torch.pi / 2., xp_prime, yp_prime
+        t = torch.linspace(0., 1., device=device)
+        if self.velocity_mode:
+            # integration
+            dt_alpha, dt_xp, dt_yp = x_alpha, x_xp, x_yp
+            dt_alpha_mid = (dt_alpha[:-1] + dt_alpha[1:]) / 2 / (self.n_t - 1)
+            alpha = torch.cat((torch.zeros([1], device=device), dt_alpha_mid.cumsum(dim=0)))
+            dt_xp_mid = (dt_xp[:-1] + dt_xp[1:]) / 2 / (self.n_t - 1)
+            xp = torch.cat((torch.zeros([1], device=device), dt_xp_mid.cumsum(dim=0)))
+            dt_yp_mid = (dt_yp[:-1] + dt_yp[1:]) / 2 / (self.n_t - 1)
+            yp = torch.cat((torch.zeros([1], device=device), dt_yp_mid.cumsum(dim=0)))
+        else:
+            # differentiation
+            dt2 = (t[1] - t[0]) * 2
+            alpha, xp, yp = x_alpha[1:-1], x_xp[1:-1], x_yp[1:-1]
+            dt_alpha = (x_alpha[2:] - x_alpha[:-2]) / dt2
+            dt_xp = (x_xp[2:] - x_xp[:-2]) / dt2
+            dt_yp = (x_yp[2:] - x_yp[:-2]) / dt2
+        return t, alpha, xp, yp, dt_alpha, dt_xp, dt_yp
