1e9eda8c25a74dfac9bcf30352f19fd9bb8051e0
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-05-30 12:08:29 +0100

    Update train_upper.py

--- train_upper.py
+++ train_upper.py
@@ -53,8 +53,16 @@ if __name__ == "__main__":
     scheduler = torch.optim.lr_scheduler.StepLR(
         optimizer, step_size=args.lr_decay_step, gamma=args.lr_decay_rate)
 
-    # alpha
-    alpha = torch.tensor(args.alphas, device=args.device)
+    # alpha, beta
+    if len(args.alphas) == 1:
+        n = int(round(args.alphas[0]))
+        alpha = torch.arange(1, n - 1, device=args.device) / n * torch.pi / 2
+        beta1 = torch.tensor((n - 1) / n * torch.pi / 2, device=args.device)
+        beta2 = torch.tensor(torch.pi / 2, device=args.device)
+    else:
+        alpha = torch.tensor(args.alphas, device=args.device)
+        beta1 = torch.tensor(args.beta1, device=args.device)
+        beta2 = torch.tensor(args.beta2, device=args.device)
 
     # path
     out_dir = Path(f"outputs/upper_small/{args.name}")
@@ -65,7 +73,7 @@ if __name__ == "__main__":
     progress_bar = trange(args.epochs)
     for epoch in progress_bar:
         u1, u2 = model.forward(alpha)
-        area = compute_area(alpha, args.beta1, args.beta2, u1, u2,
+        area = compute_area(alpha, beta1, beta2, u1, u2,
                             n_areas=args.n_areas, return_geometry=False)
         loss = -area
         if area > largest_area:
@@ -80,7 +88,7 @@ if __name__ == "__main__":
 
     # save
     u1, u2 = model.forward(alpha)
-    gg = compute_area(alpha, args.beta1, args.beta2, u1, u2,
+    gg = compute_area(alpha, beta1, beta2, u1, u2,
                       n_areas=args.n_areas, return_geometry=True)
     torch.save(model.state_dict(), f"{out_dir}/last_model.pt")
     torch.save(gg, f"{out_dir}/last_geometry.pt")
@@ -89,7 +97,7 @@ if __name__ == "__main__":
     if largest_area >= 0.:
         model.load_state_dict(torch.load(f"{out_dir}/best_model.pt"))
         u1, u2 = model.forward(alpha)
-        gg = compute_area(alpha, args.beta1, args.beta2, u1, u2,
+        gg = compute_area(alpha, beta1, beta2, u1, u2,
                           n_areas=args.n_areas, return_geometry=True)
         torch.save(gg, f"{out_dir}/best_geometry.pt")
         print("Best area:", gg["area"].item())
