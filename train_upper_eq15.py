bb8009adf75ebedf04ca5a007613270eacbc788f
Kuangdai Leng <kuangdai.leng@stfc.ac.uk>
2024-06-11 20:31:05 +0100

    Update train_upper_eq15.py

--- train_upper_eq15.py
+++ train_upper_eq15.py
@@ -1,7 +1,8 @@
 import os
+import sys
 
 import torch
 
 ns = torch.cat((torch.arange(10, 100, 10), torch.arange(100, 3001, 100)))
 for n in ns:
-    os.system(f"python train_upper.py -a {n} -N eq15_{n}")
+    os.system(f"python train_upper.py -a {n} -N eq15_{n} -s {sys.argv[1]}")
